b5e2521c6545d1f32590795cc2d3b3338112cad5
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-12-27 18:21:39 -0800

    Implemented interface

--- 02-interfaces/main.go
+++ 02-interfaces/main.go
@@ -1,7 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+
+type Truck interface {
+	LoadCargo() error
+	UnloadCargo() error
+}
+type NormalTruck struct {
+	id string
+	cargo int
+}
+
+type ElectricTruck struct {
+	id string
+	cargo int
+	battery float64
+}
+
+var (
+	ErrorsNotImplemented = errors.New("not Implemented")
+	ErrTruckNotFound = errors.New("could not process truck")
+)
+
+func (t *NormalTruck) LoadCargo() error {
+	t.cargo += 1
+	return nil
+}
+func (t *NormalTruck) UnloadCargo() error {
+	t.cargo -= 1
+	return nil
+}
+
+func (e *ElectricTruck) LoadCargo() error {
+	e.cargo += 1
+	e.battery += 1
+	return nil
+}
+func (e *ElectricTruck) UnloadCargo() error {
+		e.cargo = 0
+		e.battery += -1
+	return nil
+}
+
+func processTruck(truck Truck) error {
+	fmt.Printf("Processing truck %+v \n", truck)
+	if err := truck.LoadCargo(); err != nil {
+		return fmt.Errorf("error loading cargo: %w", err)
+	}
+
+	if err := truck.UnloadCargo(); err != nil {
+		return fmt.Errorf("error unloading cargo: %w", err)
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Interfaces")
+	nt := &NormalTruck{id: "1"}
+	et := &ElectricTruck{id: "2"}
+		err := processTruck(nt)
+		if err != nil {
+			log.Fatalf("Error processing truck: %s", err)
+		}
+
+		err = processTruck(et)
+		if err != nil {
+			log.Fatalf("Error processing truck: %s", err)
+		}
+		log.Println(nt.cargo)
+		log.Println(et.battery)
 }
